e6f62a0bee4312aeb1e8b0191eec9b05d5cb2873
DanielMP13 <dmp030902@gmail.com>
2025-04-08 19:39:09 +0200

    Add files via upload

---
+++ ejemplo5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejemplo5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora c = new Calculadora();
+        int resultado = c.Sumar(4,5);
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
+
+public class Calculadora {
+    public int Sumar(int a, int b) {
+        return a + b;
+    }
+}

78b1cb753528c7d3fd1d45861dbf347f0e18d96b
DanielMP13 <dmp030902@gmail.com>
2025-04-09 16:40:18 +0200

    Add files via upload

---
+++ ejemplo40/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ejemplo40;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dia = 3;
+
+        switch(dia)
+        {
+            case 1:
+            Console.WriteLine("Lunes");
+            break;
+            case 2:
+            Console.WriteLine("Martes");
+            break;
+            case 3:
+            Console.WriteLine("Miercoles");
+            break;
+            case 4:
+            Console.WriteLine("Jueves");
+            break;
+            case 5:
+            Console.WriteLine("Viernes");
+            break;
+            default:
+            Console.WriteLine("Día no válido");
+            break;
+        }
+    }
+}

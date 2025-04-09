3cd27af92d9699a43bf1f3a11d656c4b09d5f329
DanielMP13 <dmp030902@gmail.com>
2025-04-09 16:39:07 +0200

    Add files via upload

---
+++ ejemplo35/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejemplo35;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce una palabra: ");
+        string palabra = Console.ReadLine();
+        string palabraAlReves = "";
+
+        for (int i = palabra.Length - 1; i>=0; i--)
+        {
+            palabraAlReves += palabra[i];
+        }
+        Console.WriteLine("La palabra al revés es: " + palabraAlReves);
+    }
+}
---
+++ ejemplo37/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejemplo37;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 10;
+
+        if(numero > 5 && numero < 15)
+        {
+            Console.WriteLine("El número está entre 5 y 15.");
+        }
+        if(numero == 10 || numero == 20) 
+        {
+            Console.WriteLine("El número es 10 o 20.");
+        }
+        if(numero != 5) 
+        {
+            Console.WriteLine("El número no es 5");
+        }
+    }
+}

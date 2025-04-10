95051fbeb8c5be8005a111f07529a2cd33fff8d6
DanielMP13 <dmp030902@gmail.com>
2025-04-10 19:20:58 +0200

    Add files via upload

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] items = {"Manzana", "Pera", "Uva"};
+        List<string> fruits = new List<string>();
+
+        AddItemsToList(items, fruits);
+
+        foreach(string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+    public static void AddItemsToList(string[] array, List<string>list)
+    {
+        foreach(string item in array)
+        {
+            list.Add(item);
+        }
+    }
+}
---
+++ Ejercicio9B/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio9B;
+
+class Program
+{
+    public static List<string> frutas = new List<string>();
+    static void Main(string[] args)
+    {
+        IntroducirFrutasEnLista();
+        foreach (string f in frutas)
+        {
+            Console.WriteLine(f);
+        }
+    }
+    public static void IntroducirFrutasEnLista()
+    {
+        string seguir = "S";
+        while (seguir.ToUpper() == "S")
+        {
+            Console.Clear();
+            Console.WriteLine("Introduce una fruta: ");
+            string nuevaFruta = Console.ReadLine();
+
+            // Validación: comprobar si la fruta ya existe en la lista
+            if (frutas.Contains(nuevaFruta))
+            {
+                Console.WriteLine($"La fruta \"{nuevaFruta}\" ya esta añadida a la lista.");
+            }
+            else
+            {
+                frutas.Add(nuevaFruta);
+                Console.WriteLine($"La fruta  \"{nuevaFruta}\" se ha añadido correctamente.");
+            }
+            Console.WriteLine("Desea continuar(S/N)");
+            seguir = Console.ReadLine();
+        }
+    }
+}
